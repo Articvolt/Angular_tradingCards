daa094c088f693e489db38c4933d2a575e9107d1
Ugo DA SILVA <ugo.dasilva@ati4group.com>
2024-11-10 16:13:06 +0100

    create output to clicker count on search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {PlayingCardComponent} from "./components/playing-card/playing-card.component";
 import {Monster} from "./models/monster.model";
+import {SearchBarComponent} from "./components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,22 @@ import {Monster} from "./models/monster.model";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   imports: [
-    PlayingCardComponent
+    PlayingCardComponent,
+    SearchBarComponent
   ]
 })
 export class AppComponent {
   monster1!: Monster;
+  count: number = 0;
 
   constructor() {
     this.monster1 = new Monster();
     this.monster1.name = "Pik";
     this.monster1.hp = 40;
     this.monster1.figureCaption = "N°002 Pik"
+  }
+
+  increaseCount() {
+    this.count ++;
   }
 }
